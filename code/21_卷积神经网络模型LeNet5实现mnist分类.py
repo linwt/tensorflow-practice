81430f551b5883483629ed553727fa2121ab98ac
linwt <37769342+linwt@users.noreply.github.com>
2019-01-22 23:03:12 +0800

    update

--- code/21_卷积神经网络模型LeNet5实现mnist分类.py
+++ code/21_卷积神经网络模型LeNet5实现mnist分类.py
@@ -1,12 +1,6 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import time
 from tensorflow.examples.tutorials.mnist import input_data
-
-# 设置学习率
-learning_rate = 0.01
-# 设置训练次数
-train_steps = 1000
 
 # 定义卷积层
 def conv(input, filter_shape, bias_shape, strides_shape):
@@ -56,10 +50,13 @@ with tf.variable_scope('Conv3'):
 with tf.variable_scope('Connection'):
     connection_output = connection(conv3_output, [7*7*120, 80], [80], [-1, 7*7*120])
 
+with tf.variable_scope('dropout'):
+    dropout_output = tf.nn.dropout(connection_output, 0.7)
+    
 with tf.name_scope('Output'):
     weight = tf.Variable(tf.truncated_normal([80, 10]),dtype= tf.float32)
     bias = tf.Variable(tf.truncated_normal([10]),dtype= tf.float32)
-    y_model = tf.nn.softmax(tf.add(tf.matmul(connection_output, weight), bias))
+    y_model = tf.nn.softmax(tf.add(tf.matmul(dropout_output, weight), bias))
 
 with tf.name_scope('Loss'):
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels = y_data, logits = y_model))
@@ -71,22 +68,21 @@ with tf.name_scope('Accuracy'):
     tf.summary.scalar('The variation of the accuracy', accuracy)
 
 with tf.name_scope('Train'):
-    train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+    train_op = tf.train.AdamOptimizer(1e-3).minimize(loss)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    x_batch, y_batch = mnist.train.next_batch(50)
     writer = tf.summary.FileWriter("21_log/", sess.graph)
     merged = tf.summary.merge_all()
-    batch_x, batch_y = mnist.train.next_batch(200)
     a = []
-    for _ in range(train_steps):
-        sess.run(train_op, feed_dict={x_data: batch_x, y_data: batch_y})
-        if _ % 50 == 0:
-            print(sess.run(accuracy, feed_dict={x_data: mnist.test.images, y_data: mnist.test.labels}))
-            summary, acc = sess.run([merged, accuracy], feed_dict={x_data: mnist.test.images, y_data: mnist.test.labels})
-            a.append(acc)
-            writer.add_summary(summary, _)
+    for epoch in range(21):
+        for batch in range(n_batch):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            sess.run(train_op, feed_dict={x_data: batch_x, y_data: batch_y})
+        print('epoch:', epoch, ',accuracy:', sess.run(accuracy, feed_dict={x_data: mnist.test.images, y_data: mnist.test.labels}))
+        summary, acc = sess.run([merged, accuracy], feed_dict={x_data: mnist.test.images, y_data: mnist.test.labels})
+        a.append(acc)
+        writer.add_summary(summary, epoch)
     writer.close()
 
     # 绘制训练精度变化图
@@ -97,23 +93,24 @@ with tf.Session() as sess:
     plt.tight_layout()
     plt.show()
 
-# 0.1421
-# 0.101
-# 0.101
-# 0.0974
-# 0.101
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
-# 0.0974
+# epoch: 0 ,accuracy: 0.2409
+# epoch: 1 ,accuracy: 0.3678
+# epoch: 2 ,accuracy: 0.4367
+# epoch: 3 ,accuracy: 0.5451
+# epoch: 4 ,accuracy: 0.6318
+# epoch: 5 ,accuracy: 0.6978
+# epoch: 6 ,accuracy: 0.7291
+# epoch: 7 ,accuracy: 0.7388
+# epoch: 8 ,accuracy: 0.7763
+# epoch: 9 ,accuracy: 0.7666
+# epoch: 10 ,accuracy: 0.7734
+# epoch: 11 ,accuracy: 0.7958
+# epoch: 12 ,accuracy: 0.8025
+# epoch: 13 ,accuracy: 0.8005
+# epoch: 14 ,accuracy: 0.8065
+# epoch: 15 ,accuracy: 0.8007
+# epoch: 16 ,accuracy: 0.8091
+# epoch: 17 ,accuracy: 0.8176
+# epoch: 18 ,accuracy: 0.811
+# epoch: 19 ,accuracy: 0.8212
+# epoch: 20 ,accuracy: 0.8183
